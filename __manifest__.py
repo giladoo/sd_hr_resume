1fcb007ba9ec579f21aa08b81e640b16e3256ece
homayounfar <homayounfar@msn.com>
2025-07-13 18:46:49 +0330

    init 18.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'HR Resume',
-    'version': '17.0.1.0.2',
+    'version': '18.0.1.0.0',
     'category': 'Human Resources',
     'summary': """ """,
     'author': 'Arash Homayounfar',
